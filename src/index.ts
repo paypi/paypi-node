345a201c667016474ed9ab77b52b475ffc210044
Tom Emmerson <tom.e.j.emmerson@gmail.com>
2021-06-30 23:54:58 +0100

    Setup authenticate user and tests

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,58 @@
-import { request, gql } from 'graphql-request';
-
-const HOST = 'localhost';
+import { request, gql, GraphQLClient } from 'graphql-request';
+const HOST = 'http://localhost';
 const PORT = '8080';
 const BASE_PATH = '/graphql';
 
-function PayPI(key: string): void {
-  if (!(this instanceof PayPI)) {
-    return new PayPI(key);
+class User {
+  private subscriberSecret: string;
+  isAuthed: boolean;
+  constructor(subscriberSecret: string, isAuthed: boolean) {
+    this.subscriberSecret = subscriberSecret;
+    this.isAuthed = isAuthed;
   }
 }
+class PayPI {
+  readonly KEY: string;
+  _client: GraphQLClient;
 
-PayPI.prototype.test = function (a: string) {
-  return a;
-};
+  constructor(key: string) {
+    this.KEY = key;
+    this._client = new GraphQLClient(`${HOST}:${PORT}${BASE_PATH}`, {});
+  }
+
+  async authenticate(subscriberSecret: string): Promise<User> {
+    const authMutation = gql`
+      mutation AuthenticateClient(
+        $serviceSecret: String!
+        $subSecret: String!
+      ) {
+        checkSubscriberSecret(
+          input: {
+            serviceSecret: $serviceSecret
+            subscriptionSecret: $subSecret
+          }
+        ) {
+          isAuthed
+        }
+      }
+    `;
+
+    const variables = {
+      serviceSecret: this.KEY,
+      subSecret: subscriberSecret,
+    };
+    const data = await this._client.request(authMutation, variables, {});
+    if (data?.response?.errors?.length > 0) {
+      throw new Error(data.response.errors[0]?.message);
+    }
+
+    return new User(
+      subscriberSecret,
+      data?.response?.data?.checkSubscriberSecret?.isAuthed ?? false
+    );
+  }
+
+  makeCharge(chargeIdentifier: string, unitsUsed?: number) {}
+}
 
 export default PayPI;
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,6 +1,46 @@
 import PayPI from '../src/index';
+import { GraphQLClient } from 'graphql-request';
 
-test('adds 1 + 2 to equal 3', () => {
+function generateMockClient(response: any) {
+  class GQLClient extends GraphQLClient {
+    request<V = any>(query: any, vars: V, headers: any): Promise<any> {
+      return new Promise<any>((res, rej) => {
+        res(response);
+      });
+    }
+  }
+
+  return new GQLClient('123');
+}
+
+test('checks authed auth response', async () => {
   let s = new PayPI('123123');
-  expect(s.test(4)).toBe(4);
+  s._client = generateMockClient({
+    response: {
+      data: {
+        checkSubscriberSecret: {
+          isAuthed: true,
+        },
+      },
+    },
+    variables: {},
+  });
+  const k = await s.authenticate('1234');
+  expect(k.isAuthed).toBe(true);
+});
+
+test('checks unauthed auth response', async () => {
+  let s = new PayPI('123123');
+  s._client = generateMockClient({
+    response: {
+      data: {
+        checkSubscriberSecret: {
+          isAuthed: false,
+        },
+      },
+    },
+    variables: {},
+  });
+  const k = await s.authenticate('1234');
+  expect(k.isAuthed).toBe(false);
 });
